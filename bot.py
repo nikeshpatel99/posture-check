e728459123337bb17665a0122328388a10006429
nikeshpatel99 <44257354+nikeshpatel99@users.noreply.github.com>
2019-06-24 01:12:12 +0100

    Create basic message repeat script

--- bot.py
+++ bot.py
@@ -1,3 +1,31 @@
-with open("token.txt",'r') as f:
-    TOKEN = f.readline()
+import discord
+import asyncio
 
+with open("token.txt",'r') as f:
+    TOKEN = f.readline().strip()
+
+with open("channel.txt",'r') as f:
+    CHANNEL_ID = f.readline().strip()
+
+REMINDER_INTERVAL = 20
+
+client = discord.Client()
+
+async def posture_check():
+    await client.wait_until_ready()
+    channel = client.get_channel(int(CHANNEL_ID))
+    # TODO - get rid of True
+    while True:
+        await channel.send("Hello friends, time to check your posture!")
+        # TODO - make REMINDER_INTERVAL user defined
+        await asyncio.sleep(REMINDER_INTERVAL)
+
+@client.event
+async def on_ready():
+    print('Logged in as')
+    print(client.user.name)
+    print(client.user.id)
+    print('------')
+
+client.loop.create_task(posture_check())
+client.run(TOKEN)
