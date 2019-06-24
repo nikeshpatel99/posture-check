a2706498efcbd6b931bdc8d7626ce78ba1d6ffc0
nikeshpatel99 <44257354+nikeshpatel99@users.noreply.github.com>
2019-06-24 16:19:38 +0100

    Add user defined REMINDER_INTERVAL

--- bot.py
+++ bot.py
@@ -14,11 +14,29 @@ client = discord.Client()
 async def posture_check():
     await client.wait_until_ready()
     channel = client.get_channel(int(CHANNEL_ID))
-    # TODO - get rid of True
-    while True:
+    while not client.is_closed():
         await channel.send("Hello friends, time to check your posture!")
-        # TODO - make REMINDER_INTERVAL user defined
         await asyncio.sleep(REMINDER_INTERVAL)
+
+@client.event
+async def on_message(message):
+    global REMINDER_INTERVAL
+    if message.author == client.user:
+        return
+    channel = message.channel
+    if message.content.startswith('!'):
+        if message.content == "!hi" or message.content == "!hello":
+            await channel.send("Hello %s!"%message.author.name)
+        elif message.content.startswith('!interval'):
+            try:
+                REMINDER_INTERVAL = int(message.content.split(' ')[1])
+                if REMINDER_INTERVAL != 1:
+                    await channel.send("The interval is now set as '%i' seconds."%REMINDER_INTERVAL)
+                else:
+                    await channel.send("The interval is now set as 1 second.")
+            except:
+                await channel.send("I don't recognise that as a number :(")
+
 
 @client.event
 async def on_ready():
